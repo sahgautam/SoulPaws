9fb5e054ff94156b0710796fbe4403d92b222ab1
subhashritgithub <[200~subhashmahato5268@gmail.com~>
2023-07-12 20:21:37 +0545

    pushing my files in branch

--- my_soulpaws_app/lib/ChangePassword.dart
+++ my_soulpaws_app/lib/ChangePassword.dart
@@ -4,19 +4,5 @@ class User {
   String email;
   String password;
 
-  User({
-    required this.id,
-    required this.name,
-    required this.email,
-    required this.password,
-  });
 
-  void changePassword(String currentPassword, String newPassword) {
-    if (password == currentPassword) {
-      password = newPassword;
-      print('Password changed successfully.');
-    } else {
-      print('Invalid current password. Password change failed.');
-    }
-  }
 }
