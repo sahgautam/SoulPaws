8f06a7d0e1e5aac9995ecd7e69a46f4365d5daac
subhashritgithub <[200~subhashmahato5268@gmail.com~>
2023-07-14 19:19:02 +0545

    pushing my files in branch

--- my_soulpaws_app/lib/ForgetPassword.dart
+++ my_soulpaws_app/lib/ForgetPassword.dart
@@ -8,4 +8,30 @@ import '../../ViewModel/auth_viewmodel.dart';
 class ForgetPasswordScreen extends StatefulWidget {
   const ForgetPasswordScreen({Key? key}) : super(key: key);
 
+  @override
+  _ForgetPasswordScreenState createState() => _ForgetPasswordScreenState();
+}
+
+class _ForgetPasswordScreenState extends State<ForgetPasswordScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  void resetPassword() async {
+    _ui.loadState(true);
+    try {
+      await _auth.resetPassword(_emailController.text).then((value) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text("Password reset link has been sent to your email.")));
+        Navigator.of(context).pop();
+      }).catchError((e) {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: Text(e.message.toString())));
+      });
+    } catch (err) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(err.toString())));
+    }
+    _ui.loadState(false);
+  }
+
+  late GlobalUIViewModel _ui;
+  late AuthViewModel _auth;
  
