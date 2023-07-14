96665040c84279bea31b983f98fd648bab5c8c0e
subhashritgithub <[200~subhashmahato5268@gmail.com~>
2023-07-14 19:38:21 +0545

    pushing my files in branch

--- my_soulpaws_app/lib/PetDescription.dart
+++ my_soulpaws_app/lib/PetDescription.dart
@@ -7,3 +7,10 @@ class PetDescription {
     required this.description,
   });
 }
+
+void main() {
+  PetDescription petDescription = PetDescription(
+    petId: '1',
+    description: 'Meet Max! He is a friendly and playful Labrador Retriever. Max loves going on long walks and playing fetch. He is great with kids and other pets. Max is fully vaccinated and ready to find his forever home.',
+  );
+  
