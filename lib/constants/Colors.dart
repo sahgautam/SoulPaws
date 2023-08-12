a466a1421e0f67bf67ce0a0c4d3481652e625e63
sahgautam <gshah26688@gmail.com>
2023-08-12 10:25:36 +0545

    pushing my files in branch

---
+++ lib/constants/Colors.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const Color textColor = Color.fromRGBO(255, 255, 255, 1);
+const Color BackColor = Color.fromRGBO(191, 134, 143, 30);
+const Color iconColor = Color.fromRGBO(0, 0, 0, 1);
