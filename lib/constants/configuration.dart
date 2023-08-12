6c0848e80739cbfb49a621ba84f47a0e350a6e40
sahgautam <gshah26688@gmail.com>
2023-08-12 10:28:18 +0545

    pushing my files in branch

---
+++ lib/constants/configuration.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+List<BoxShadow> shadowList = [
+  BoxShadow(color: Colors.grey.shade300, blurRadius: 30, offset: Offset(0, 10))
+];
+List<Map> categories = [
+  {"name": "Cats", "iconPath": "Assets/cat_icon.png"},
+  {"name": "Dogs", "iconPath": "Assets/dog_icon.png"}
+];
