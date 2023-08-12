2f05f1316f4dacfb97bf427fbe7cf8b27b848337
sahgautam <gshah26688@gmail.com>
2023-08-12 12:44:51 +0545

    pushing my files in branch

---
+++ lib/UserScreens/veterinaty.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class Veterinary extends StatelessWidget {
+  final List<Map<String, String>> links = [
+  {
+  'name': 'Vet For You',
+  'url':
+  'https://www.google.com/search?q=Vet+or+you&tbm=lcl&sxsrf=AJOqlzVrA-J0yv0Vqr1-1vLi3rEmgCKy7Q%3A1677635387910&ei=O6_-Y9uUN9bw4-EP1a2dgAU&ved=0ahUKEwibnu_qzrn9AhVW-DgGHdVWB1AQ4dUDCAk&uact=5&oq=Vet+or+you&gs_lcp=Cg1nd3Mtd2l6LWxvY2FsEAMyBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjIGCAAQFhAeMgYIABAWEB4yBggAEBYQHjoECCMQJzoFCAAQkQI6CwgAEIAEELEDEIMBOggIABCABBCxAzoICAAQsQMQgwE6BAgAEEM6CggAELEDEIMBEEM6BwgAEMkDEEM6BQgAEJIDOggIABCABBCSAzoFCAAQgAQ6CggAEIAEEBQQhwJQAFjwFmCkGWgAcAB4AIABrAGIAawMkgEEMC4xMJgBAKABAcABAQ&sclient=gws-wiz-local#rlfi=hd:;si:4503408657182793396,l,CgpWZXQgb3IgeW91SNrX36Csj4CACFoWEAAQARACGAAYAiIKdmV0IG9yIHlvdZIBD2FuaW1hbF9ob3NwaXRhbJoBI0NoWkRTVWhOTUc5blMwVkpRMEZuU1VSRE1tTlBNRVJuRUFFqgESEAEqDiIKdmV0IG9yIHlvdSgA;mv:[[27.7471948,85.5326463],[27.623801999999998,85.2781183]]'
+},
