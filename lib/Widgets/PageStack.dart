d37979dad3353eb09de2467848ddfa4c521ca3e1
sahgautam <gshah26688@gmail.com>
2023-08-12 12:54:56 +0545

    pushing my files in branch

---
+++ lib/Widgets/PageStack.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../UserScreens/subpages/subScreen.dart';
+import 'AppColumn.dart';
+
+class CreatePageStack extends StatelessWidget {
+  final int index;
+  final String dogName;
+  final String dogDescription;
+  final String imageLink;
+  final String breed;
+  final String color;
+  final int price;
