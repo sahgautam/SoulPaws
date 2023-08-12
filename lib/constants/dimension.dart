0f72a499242b214212261b2aa956784740b87a16
sahgautam <gshah26688@gmail.com>
2023-08-12 11:37:28 +0545

    pushing my files in branch

--- lib/constants/dimension.dart
+++ lib/constants/dimension.dart
@@ -6,4 +6,11 @@ class Dimension {
   static double? screenHeight;
   static double? leftMargin;
   static double? rightMargin;
-  
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenHeight = _mediaQueryData!.size.height;
+    screenWidth = _mediaQueryData!.size.width;
+    leftMargin = _mediaQueryData!.size.width / 4;
+    rightMargin = _mediaQueryData!.size.width / 4;
+  }
+}
