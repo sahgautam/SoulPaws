2835425c23bef40bbfa4f0534678ea687aa1816c
sahgautam <gshah26688@gmail.com>
2023-08-12 12:57:44 +0545

    pushing my files in branch

--- lib/Widgets/PageStack.dart
+++ lib/Widgets/PageStack.dart
@@ -23,34 +23,49 @@ class CreatePageStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-        onTap: () {
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => SubScreen(index, dogName, dogDescription,
-                  imageLink, price, breed, color)));
-    },
-    child: Center(
-    child: Container(
-    margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
-    child: Stack(alignment: Alignment.topCenter, children: [
-    Expanded(
-    child: Container(
-    height: 150,
-    decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(30),
-    color: index.isEven
-    ? Color.fromRGBO(255, 255, 187, 10)
-        : Color.fromRGBO(195, 223, 195, 10),
-    image: DecorationImage(
-    fit: BoxFit.contain, image: NetworkImage(imageLink))),
-    ),
-    ),
-    Container(
-    height: 100,
-    width: 200,
-    margin: EdgeInsets.only(top: 100.0),
-    decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(30),
-    color: Colors.white,
-    ),
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => SubScreen(index, dogName, dogDescription,
+                    imageLink, price, breed, color)));
+      },
+      child: Center(
+        child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Stack(alignment: Alignment.topCenter, children: [
+            Expanded(
+              child: Container(
+                height: 150,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: index.isEven
+                        ? Color.fromRGBO(255, 255, 187, 10)
+                        : Color.fromRGBO(195, 223, 195, 10),
+                    image: DecorationImage(
+                        fit: BoxFit.contain, image: NetworkImage(imageLink))),
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 200,
+              margin: EdgeInsets.only(top: 100.0),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+              ),
+              child: Center(
+                child: AppColumn(
+                  Name: dogName,
+                  breed: breed,
+                  color: color,
+                  price: price,
+                ),
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
