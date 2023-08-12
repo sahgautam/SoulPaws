21d1bbf40c91490cf6adb12392a93fb809ddc3e0
sahgautam <gshah26688@gmail.com>
2023-08-12 12:48:48 +0545

    pushing my files in branch

--- lib/ViewModel/AdoptViewModel.dart
+++ lib/ViewModel/AdoptViewModel.dart
@@ -2,3 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:soulpaws/model/AdoptModel.dart';
 import 'package:soulpaws/repositories/adopt_repo.dart';
+
+class AdoptionViewModel with ChangeNotifier {
+  AdoptionRepo _adoptionRepo = AdoptionRepo();
+  Stream<QuerySnapshot<AdoptionModel>>? _adoptdog;
+  //_underscore rakheko variable aaru cls le use garna mildain a
+  Stream<QuerySnapshot<AdoptionModel>>? get adoptDog => _adoptdog;
