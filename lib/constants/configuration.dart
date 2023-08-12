39165980c45400a1a1bc60f993eda811290bf46b
sahgautam <gshah26688@gmail.com>
2023-08-12 10:30:06 +0545

    pushing my files in branch

--- lib/constants/configuration.dart
+++ lib/constants/configuration.dart
@@ -8,3 +8,10 @@ List<Map> categories = [
   {"name": "Cats", "iconPath": "Assets/cat_icon.png"},
   {"name": "Dogs", "iconPath": "Assets/dog_icon.png"}
 ];
+List<Color> CarouselColor = [
+  Colors.blue.shade300,
+  Colors.blueGrey.shade300,
+  Colors.green.shade300,
+  Colors.amber.shade300,
+  Colors.deepPurple.shade300
+];
