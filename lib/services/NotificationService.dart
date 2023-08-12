78d9424c0594308cbb771021b9aef168d99b9b5e
sahgautam <gshah26688@gmail.com>
2023-08-12 11:42:38 +0545

    pushing my files in branch

--- lib/services/NotificationService.dart
+++ lib/services/NotificationService.dart
@@ -6,3 +6,8 @@ import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:http/http.dart' as http;
 import 'package:path_provider/path_provider.dart';
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+  
