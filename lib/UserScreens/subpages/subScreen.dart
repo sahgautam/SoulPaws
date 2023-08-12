45f355e50d1114e37c9a8f6ff4fd1a0fee6a07f8
sahgautam <gshah26688@gmail.com>
2023-08-12 12:03:19 +0545

    pushing my files in branch

--- lib/UserScreens/subpages/subScreen.dart
+++ lib/UserScreens/subpages/subScreen.dart
@@ -5,3 +5,13 @@ final data = FavoriteDogModel(
   imageUrl: widget.imageLink!,
   breed: widget.breed!,
 );
+
+db.collection("adopt").add(data.toJson()).then((value) {
+print("Added Data with ID: ${value.id}");
+ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+content: Text(
+"Dog Applied for Adoption",
+style: TextStyle(color: Colors.white),
+)));
+});
+}
