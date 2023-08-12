502ef9ea871dfbe0b1b1ef942a04573f45969f14
sahgautam <gshah26688@gmail.com>
2023-08-12 11:36:25 +0545

    pushing my files in branch

---
+++ lib/constants/dimension.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+
+class Dimension {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenWidth;
+  static double? screenHeight;
+  static double? leftMargin;
+  static double? rightMargin;
+  
