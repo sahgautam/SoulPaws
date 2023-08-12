1de5006c5f4fe61859d0f7e775bd003b4744c965
sahgautam <gshah26688@gmail.com>
2023-08-12 12:37:04 +0545

    pushinh my files in branch

--- lib/UserScreens/Payment.dart
+++ lib/UserScreens/Payment.dart
@@ -10,3 +10,15 @@ class KhaltiPayment extends StatefulWidget {
 class _KhaltiPaymentState extends State<KhaltiPayment> {
   TextEditingController phoneController = new TextEditingController();
   TextEditingController pinCodeController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+      appBar: AppBar(
+      title: Text(
+      "Khalti",
+      style: TextStyle(color: Colors.white),
+    ),
+    centerTitle: true,
+    backgroundColor: Colors.purple,
+    ),
